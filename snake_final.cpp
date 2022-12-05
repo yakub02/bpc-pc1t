40136cac2d7dfc3b88eff6a27547ed060a9f4aa5
yakub02 <114614165+yakub02@users.noreply.github.com>
2022-12-05 23:56:59 +0100

    FINAL VERZE

--- snake_final.cpp
+++ snake_final.cpp
@@ -1,4 +1,4 @@
-#include "snake_final.h"
+﻿#include "snake_final.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,53 +15,56 @@
 #define LEFT 75 //doleva
 #define RIGHT 77 //doprava
 
-int i, j, field[N][M]; //field - herni pole kde se pohybuje had
-int x, y, Gy, head, tail, game, points, score, highscore; // x,y = cords of snake // game = game loop aby bezela hra dokola //points = body
-int a, b; // nahodne pozice pointu
-int key, dir; //pohyb
+
+int field[N][M]; //pole kde se pohybuje had
+int x, y, head, tail, game, points, score , highscore, dir; // x,y - pozice hada // game = game loop aby bezela hra dokola //points = body
 char plname[20], nplname[20], cha, c; //maximalni pocet znaku
+
 FILE* f;
 
-void snakeProperties() { //inicializace hada
-    {
-        printf("\nVitejte ve hre Had \n\n(Pokracujte stisknutim jakekoliv klavesy...)");
-        int i, j, px;
-        FILE* info;
-        info = fopen("tabulka.txt", "a+"); //otevreni souboru "tabulka.txt"
-        getch();
-        system("cls");
-        printf("Zadej sve jmeno\n");
-        scanf("%[^\n]", plname); //zadani jmena hrace
-        for (j = 0; plname[j] != '\0'; j++) {
-            nplname[0] = toupper(plname[0]);
-            if (plname[j - 1] == ' '){
-                nplname[j] = toupper(plname[j]);
-                nplname[j - 1] = plname[j - 1];
-            }
-            else nplname[j] = plname[j];
-        }
-        nplname[j] = '\0';
-        fprintf(info, "\n");
-        fclose(info);
-        system("cls");
-    }
+void snakeVlastnosti() { //inicializace hada
+    int z, i, j, px; //pomocna promenna
+    FILE* hraci;
+    printf("\nVitejte ve hre Had \n\n(Pokracujte stisknutim jakekoliv klavesy...)");
+    hraci = fopen("tabulka.txt", "a+"); //otevreni souboru "tabulka.txt"
+    getch();
+    system("cls");
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    printf("Zadej sve jmeno (Hra se po zadani jmena automaticky...)\n");
+    scanf("%[^\n]", plname); //zadani jmena hrace
+    for (j = 0; plname[j] != '\0'; j++) {
+        nplname[0] = toupper(plname[0]);
+        if (plname[j - 1] == ' ') {
+            nplname[j] = toupper(plname[j]);
+            nplname[j - 1] = plname[j - 1];
+        }
+        else nplname[j] = plname[j];
+    }
+    nplname[j] = '\0';
+    fprintf(hraci, "\n");
+    fclose(hraci);
+    system("cls");
+
+    f = fopen("tabulka.txt", "r");
+    fscanf(f, "%d", &highscore);
+    fclose(f);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
             field[i][j] = 0;
         }
     }
 
-    x = N / 2; y = M / 2; Gy = y; head = 5; tail = 1, game = 0; points = 0; dir = RIGHT, score = 0; //pozice hada uprostred pole, //kdyz game = 0, //smer kterzm had zacina
+    x = N / 2; y = M / 2; z = y; head = 5; tail = 1, game = 0; points = 0;score = 0, dir = RIGHT; //pozice hada uprostred pole, //kdyz game = 0, //smer kterzm had zacina
 
-    for (i = 0; i < head; i++) {
+    for (int i = 0; i < head; i++) {
 
-        Gy++;
-        field[x][Gy - head] = i + 1;
+        z++;
+        field[x][z - head] = i + 1;
     }
 }
 
-void printBorder() {
+void print() {
 
     for (int i = 0; i <= M + 1; i++) {  //horni radek
         printf("%c", 43);
@@ -87,10 +90,12 @@ void printBorder() {
         printf("%c", 43);
     }
     printf("\n  Aktualni skore : %d", score);
+    printf("\n  Nejvyssi skore : ");printf("\033[0;31m");printf("%d", highscore);
+    printf("\033[0m"); //zmena na puvodni barvu
     printf("\n");
 }
 
-void ResetScreenPosition() {
+void resetovaniPozice() {
 
     HANDLE hOut;
     COORD Position;
@@ -101,6 +106,7 @@ void ResetScreenPosition() {
 }
 
 void Random() { //nahodna pozice pointu v poli
+    int a, b;
     srand(time(0)); //nahodna pozice pro point 
     a = 1 + rand() % 18; //18 kvuli 0 a 20 pozici, kde by byl point v borderu herniho pole
     b = 1 + rand() % 78;
@@ -114,16 +120,17 @@ void Random() { //nahodna pozice pointu v poli
 void konecHry() {
     printf("\a");
     Sleep(1500);
-    system("Cls"); //vymaze command prompt
+    system("cls"); //vymaze command prompt
 
     f = fopen("tabulka.txt", "a"); //otevre txt ve write mode
     fprintf(f, "%s %d", nplname, score);
     fclose(f);
-    
+
     system("cls");
     printf("\033[0;31m"); //zmena na cervenou barvu
-    printf("\n\n    KONEC HRY\n\n");
+    printf("\n\n    GAME OVER\n\n");
     printf("    SKORE :  %d \n\n", score);
+    printf("Tabulka hracu je v textovem souboru.\n");
     printf("\033[0m"); //zmena na puvodni barvu
     system("pause");
     game = 1;
@@ -138,6 +145,7 @@ int Hit() { //registrace stisku klavesy
 
 //nefunknci pohyb pres sipky
 void movement() {
+    int key;
     key = Hit();
     //kontrola, aby had nemohl otocit zpet do sveho tela
     if ((key == LEFT && dir != RIGHT) ||
@@ -182,8 +190,8 @@ void movement() {
 }
 
 void tailremover() {
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
             if (field[i][j] == tail) { //konstantni odstranovani konce hada 
                 field[i][j] = 0;
             }
@@ -194,13 +202,14 @@ void tailremover() {
 
 void main() {
 
-    snakeProperties();
+    snakeVlastnosti();
 
     while (game == 0) {
-        printBorder();
-        ResetScreenPosition();
+        print();
+        resetovaniPozice();
         Random();
         movement();
         tailremover();
+        //sleep(99);
     }
 }
